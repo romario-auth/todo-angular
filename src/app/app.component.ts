220567d9e9dd239f69e402059d6395cd8143fe72
rromario <romario.damasceno@zukk.com.br>
2021-08-15 19:30:52 -0300

    Exibir, alterar status e excluir os Todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  public todos: Todo[] = [];
+  public title: String = "Minhas tarefas";
+
+  constructor() {
+    this.todos.push(new Todo(1, "Limpar a cozinha", true));
+    this.todos.push(new Todo(2, "Ir ao mercador", false));
+    this.todos.push(new Todo(3, "Fazer pão de queijo", false));
+  }
+
+  remove (todo: Todo)
+  {
+    const index = this.todos.indexOf(todo);
+
+    if(index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo)
+  {
+    todo.done = true;
+
+  }
+
+  markAsUndone(todo: Todo)
+  {
+    todo.done = false;
+  }
 
 }
